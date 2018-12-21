5b3c3b1b81379df06f3fe1a9b4bd840d8f74ccae
dstocco <dstocco@olpays.com>
2018-12-20 22:40:24 -0300

    added loadTemplate and write functions to helpers file.

--- helpers.js
+++ helpers.js
@@ -1,5 +1,7 @@
 const mkdirp = require('mkdirp');
 const path = require('path');
+const fs = require('fs');
+
 
 
 // Creo que esto le da permisos a los directorios. Ver como Hace la magia.
@@ -7,7 +9,9 @@ const MODE_0666 = parseInt('0666', 8);
 const MODE_0755 = parseInt('0755', 8);
 
 module.exports = {
-    mkdir
+    mkdir,
+    loadTemplate,
+    write
 };
 
 
@@ -16,4 +20,27 @@ function mkdir (base, dir) {
 
     console.log('   \x1b[36mcreate\x1b[0m : ' + loc + path.sep);
     mkdirp.sync(loc, MODE_0755);
+}
+
+
+
+function loadTemplate (file) {
+    const contents = fs.readFileSync(path.join(__dirname, '..', 'templates/modules', (file + '.ejs')), 'utf-8');
+    const locals = Object.create(null);
+
+    function render () {
+        return ejs.render(contents, locals, {
+            escape: util.inspect
+        })
+    }
+
+    return {
+        locals: locals,
+        render: render
+    }
+}
+
+function write (file, str, mode) {
+    fs.writeFileSync(file, str, { mode: mode || MODE_0666 });
+    console.log('   \x1b[36mcreate\x1b[0m : ' + file)
 }
